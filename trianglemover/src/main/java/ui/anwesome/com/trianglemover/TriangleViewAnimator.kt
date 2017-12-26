95d863c0c095e89e5c7e4eff9c5ddb3254d69bf5
Anwesh43 <anweshthecool0@gmail.com>
2017-12-26 13:01:15 +0530

    addint TriangleViewAnimator

---
+++ trianglemover/src/main/java/ui/anwesome/com/trianglemover/TriangleViewAnimator.kt
@@ -0,0 +1,32 @@
+package ui.anwesome.com.trianglemover
+
+import android.view.View
+
+/**
+ * Created by anweshmishra on 26/12/17.
+ */
+class TriangleViewAnimator(var view:View,var animated:Boolean = false) {
+    var startAnimation:()->Unit = {
+        if(!animated) {
+            animated = true
+            view.postInvalidate()
+        }
+    }
+    var stopAnimation:()->Unit = {
+        if(!animated) {
+            animated = true
+        }
+    }
+    fun update(updatecb:()->Unit) {
+        if(animated) {
+            updatecb()
+            try {
+                Thread.sleep(50)
+                view.invalidate()
+            }
+            catch(ex:Exception) {
+
+            }
+        }
+    }
+}
