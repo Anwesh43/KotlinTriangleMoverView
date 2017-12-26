dcdb41c80899f2c07f6aa79a7b838bb34e4a49d6
Anwesh43 <anweshthecool0@gmail.com>
2017-12-26 12:53:31 +0530

    adding AnimatorQueue

---
+++ trianglemover/src/main/java/ui/anwesome/com/trianglemover/AnimatorQueue.kt
@@ -0,0 +1,29 @@
+package ui.anwesome.com.trianglemover
+
+import java.util.*
+
+/**
+ * Created by anweshmishra on 26/12/17.
+ */
+class AnimatorQueue(var scale:Float = 0f,var dir:Float = 0f) {
+    var animations:LinkedList<(Float)->Unit> = LinkedList()
+    fun addAnimation(animation:(Float)->Unit) {
+        animations.add(animation)
+    }
+    fun update(stopcb:()->Unit) {
+        if(animations.size > 0) {
+            scale += 0.1f*dir
+            animations[0].invoke(scale)
+            if(scale >= 1) {
+                scale = 0f
+                dir = 1f
+                animations.removeFirst()
+                if(animations.size == 0) {
+                    dir = 0f
+                    stopcb()
+                }
+            }
+        }
+
+    }
+}
