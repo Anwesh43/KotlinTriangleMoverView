139c67165a93a24ab4201772d32cddb19825331d
Anwesh43 <anweshthecool0@gmail.com>
2017-12-26 13:55:25 +0530

    having fullscreen

--- app/src/main/java/ui/anwesome/com/kotlintrianglemoverimpl/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintrianglemoverimpl/MainActivity.kt
@@ -1,7 +1,10 @@
 package ui.anwesome.com.kotlintrianglemoverimpl
 
+import android.app.Activity
+import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.trianglemover.TriangleMoverView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TriangleMoverView.create(this)
+        fullScreen()
+        fixPortraitOrientation()
+        hideActionBar()
     }
+}
+fun Activity.fullScreen() {
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+}
+fun Activity.fixPortraitOrientation() {
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+}
+fun AppCompatActivity.hideActionBar() {
+    supportActionBar?.hide()
 }
