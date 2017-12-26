fdf67240e9d455caa01893364ca83a547f975a28
Anwesh43 <anweshthecool0@gmail.com>
2017-12-26 13:41:22 +0530

    using TriangleMoverView in Android Activity

--- app/src/main/java/ui/anwesome/com/kotlintrianglemoverimpl/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlintrianglemoverimpl/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlintrianglemoverimpl
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.trianglemover.TriangleMoverView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriangleMoverView.create(this)
     }
 }
