e9334fb45cd279781b5cc2f9f344c6247e46c004
Anwesh43 <anweshthecool0@gmail.com>
2017-12-26 12:45:43 +0530

    adding DirectionUtil

---
+++ trianglemover/src/main/java/ui/anwesome/com/trianglemover/DirectionUtil.kt
@@ -0,0 +1,37 @@
+package ui.anwesome.com.trianglemover
+
+/**
+ * Created by anweshmishra on 26/12/17.
+ */
+class DirectionUtil {
+    companion object {
+        fun angle_from_xy(x:Float,y:Float,px:Float,py:Float):Float {
+            val x_diff = (x - px).toDouble()
+            val y_diff = (y - py).toDouble()
+            if(x_diff == 0.0) {
+                if(y_diff >=0) {
+                    return 90f
+                }
+                return 270f
+            }
+            else {
+                var deg = (Math.atan(y_diff/x_diff)*(180/(Math.PI))).toFloat()
+                if(deg >= 0) {
+                    if(x_diff > 0) {
+                        return deg
+                    }
+                    return deg+180
+                }
+                else {
+                    if(x_diff > 0) {
+                        return 360-deg
+                    }
+                    else {
+                        return 180-deg
+                    }
+                }
+            }
+            return 0f
+        }
+    }
+}
